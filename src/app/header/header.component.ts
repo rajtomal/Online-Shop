850e096f2de13e03796925316bc1b81396d7b37f
Tomal Ahmed Raj <sj.tomalahmedraj@gmail.com>
2023-07-10 18:15:19 +0600

    user-profile

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef } from '@angular/core';
+import { tick } from '@angular/core/testing';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-
+  menuType: string = 'default';
+  sellerName: string = '';
+  constructor(private route: Router) { }
+  ngOnInit(): void {
+    this.route.events.subscribe((val: any) => {
+      if (val.url) {
+        if (localStorage.getItem('seller') && val.url.includes('seller')) {
+          console.log(" login seller")
+          this.menuType = 'seller';
+          if (localStorage.getItem('seller')) {
+            let sellerStore: any = localStorage.getItem('seller');
+            let sellerData: any = sellerStore && JSON.parse(sellerStore)[0];
+            this.sellerName = sellerData.name;
+            console.log(sellerData, 'hi')
+          }
+        } else {
+          console.log("not seller")
+          this.menuType = 'default';
+        }
+      }
+    })
+  }
+  logOut() {
+    localStorage.removeItem('seller');
+    this.route.navigate(['/']);
+  }
 }
--- src/app/services/seller.service.ts
+++ src/app/services/seller.service.ts
@@ -1,5 +1,5 @@
 import { EventEmitter, Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
+import { HttpClient } from '@angular/common/http'
 import { SignUp, logIn } from '../data-type';
 import { Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
@@ -8,31 +8,31 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class SellerService {
-  isSellerLoggedIn= new BehaviorSubject<boolean>(false);
+  isSellerLoggedIn = new BehaviorSubject<boolean>(false);
   isLogInError = new EventEmitter<boolean>(false)
 
-  constructor(private http:HttpClient, private router:Router) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
 
 
   //post sign up data
-  userSignUp(data:SignUp){
-    this.http.post('http://localhost:3000/seller', data,{observe:'response'}).subscribe((result)=>{
+  userSignUp(data: SignUp) {
+    this.http.post('http://localhost:3000/seller', data, { observe: 'response' }).subscribe((result) => {
       // Behavior true
       this.isSellerLoggedIn.next(true);
       // localStorage Save Data
-      localStorage.setItem('seller',JSON.stringify(result.body));
+      localStorage.setItem('seller', JSON.stringify(result.body));
       // path defined
       this.router.navigate(['seller-home']);
     });
   }
-  userLogIn(data:logIn){
-    this.http.post('http://localhost:3000/login', data , {observe:'response'}).subscribe(()=>{
+  userLogIn(data: logIn) {
+    this.http.post('http://localhost:3000/login', data, { observe: 'response' }).subscribe(() => {
 
     })
   }
-  reloadSeller(){
-    if(localStorage.getItem('seller')){
+  reloadSeller() {
+    if (localStorage.getItem('seller')) {
       this.isSellerLoggedIn.next(true);
       this.router.navigate(['seller-home']);
     }
@@ -40,15 +40,15 @@ export class SellerService {
 
   // login data
 
-  logIn(data:logIn){
+  logIn(data: logIn) {
     // console.log(data)
-    this.http.get(`http://localhost:3000/seller?email=${data.email}&password=${data.password}`, {observe:'response'}).subscribe((result:any)=>{
-      if(result && result.body && result.body.length){
-        localStorage.setItem('seller',JSON.stringify(result.body));
-      // path defined
-      this.router.navigate(['seller-home']);
-      console.log("log In sucess")
-      }else{
+    this.http.get(`http://localhost:3000/seller?email=${data.email}&password=${data.password}`, { observe: 'response' }).subscribe((result: any) => {
+      if (result && result.body && result.body.length) {
+        localStorage.setItem('seller', JSON.stringify(result.body));
+        // path defined
+        this.router.navigate(['seller-home']);
+        console.log("log In sucess")
+      } else {
         console.log("log In Fail")
         this.isLogInError.emit(true)
       }
