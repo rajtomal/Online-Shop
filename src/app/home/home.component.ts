27c3edab757dcc5e0de25937f6996bfff20afca3
Tomal Ahmed Raj <sj.tomalahmedraj@gmail.com>
2023-07-16 18:23:03 +0600

    16-07-2023

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../services/products.service';
+import { products } from '../data-type';
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent {  
+  trendyProductsItem: undefined | products[];
 
+  cartIcon=faCartPlus;
+
+  constructor(private Product: ProductsService){};
+
+  ngOnInit():void {
+    this.Product.trendyProducts().subscribe((result) => {
+      console.log(result)
+      this.trendyProductsItem = result
+    })
+  }
+  imagesAll = [
+    {
+      images : 'https://azse77seaprodsa.blob.core.windows.net/b2b-dr-pickaboocdn/media/dcastalia_hybridslider/image/big_banner_6_.jpg'
+    },
+    {
+      images : 'https://azse77seaprodsa.blob.core.windows.net/b2b-dr-pickaboocdn/media/dcastalia_hybridslider/image/Nord_CE_3_Lite_5G_Series_big_banner_1__1.jpg'
+    },
+    {
+      images : 'https://azse77seaprodsa.blob.core.windows.net/b2b-dr-pickaboocdn/media/dcastalia_hybridslider/image/Big_Banner_5_1_.jpg'
+    },
+    {
+      images : 'https://azse77seaprodsa.blob.core.windows.net/b2b-dr-pickaboocdn/media/dcastalia_hybridslider/image/Big_Banner_9_1_.jpg'
+    }
+  ]
 }
