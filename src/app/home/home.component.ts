cc34715d506b4f7dad3ec2d2ca1efbc86d99e658
Tomal Ahmed Raj <sj.tomalahmedraj@gmail.com>
2024-10-29 12:28:12 +0600

    loader

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../services/products.service';
 import { products } from '../data-type';
 import { Router } from '@angular/router';
+import { finalize } from 'rxjs';
 // import Slider from '../../assets/images/slider-1.jpg';
 
 @Component({
@@ -9,38 +10,48 @@ import { Router } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {  
+export class HomeComponent {
   trendyProductsItem: undefined | products[];
+  loading: boolean = false;
 
+  constructor(private Product: ProductsService, private route: Router) { };
 
-  constructor(private Product: ProductsService, private route:Router){};
+  ngOnInit(): void {
+    this.loading = true
+    this.Product.trendyProducts().pipe(finalize(() => this.loading = false)).subscribe({
+      next: (result) => {
+        console.log(result)
+        this.trendyProductsItem = result
+        console.log(this.trendyProductsItem)
+      },
+      error: (err) => {
 
-  ngOnInit():void {
-    this.Product.trendyProducts().subscribe((result) => {
+      }
+
       // console.log(result)
-      this.trendyProductsItem = result
-      console.log(this.trendyProductsItem)
+      // this.trendyProductsItem = result
+      // console.log(this.trendyProductsItem)
     })
   }
   imagesAll = [
     {
-      images : '../../assets/images/slider-1.jpg'
+      images: '../../assets/images/slider-1.jpg'
     },
     {
-      images : '../../assets/images/slider-2.jpg'
+      images: '../../assets/images/slider-2.jpg'
     },
     {
-      images : '../../assets/images/slider-3.jpg'
+      images: '../../assets/images/slider-3.jpg'
     },
     {
-      images : '../../assets/images/slider-4.jpg'
+      images: '../../assets/images/slider-4.jpg'
     }
   ]
-  moreDetails(id:number){
+  moreDetails(id: number) {
     // console.log(id)
     this.route.navigate([`product-details/${id}`])
   }
-  addToCart(num:number){
+  addToCart(num: number) {
     console.log(num)
   }
 }
